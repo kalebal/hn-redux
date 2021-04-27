64db5c1fc45c6a324c5ee48031b1a01e3b9e58a9
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 18:48:55 -0400

    Move todo comments to file

--- src/features/search/PastSearches.js
+++ src/features/search/PastSearches.js
@@ -4,6 +4,9 @@ import { selectSearch } from './searchSlice';
 
 export default function PastSearches () {
   const pastSearches = useSelector(selectSearch);
+  // TODO: clicking on a past search changes search input value
+  // and displays results
+  // To consider: cache past results by search query for faster load time on click
   return (
     <section>
       <h3>Past Searches</h3>
