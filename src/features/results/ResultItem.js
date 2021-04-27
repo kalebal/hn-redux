057cca9fe16a9498a2acaa35c83167f4006d94a4
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 18:51:00 -0400

    Init result list item component

---
+++ src/features/results/ResultItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ResultItem({ data }) {
+  // Eventual ToDos:
+  // li's link to article
+  // clicking on title expands to show more info
+  console.log(data);
+  return (
+    <li key={data.objectID}>
+      <a href={data.url}>{data.title}</a>
+      <p> by: {data.author} </p>
+    </li>
+  )
+}
