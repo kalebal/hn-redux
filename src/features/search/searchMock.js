b9db7974465bed3978a7c36845a6b5927b022cb6
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 18:17:08 -0400

    Deleted asyncMock, switched to Postman

--- src/features/search/searchMock.js
+++
@@ -1,6 +0,0 @@
-// A mock function to mimic making an async request for data
-export function fetchData(str = 'search string') {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: str }), 500)
-  );
-}
