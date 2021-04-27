d87fb65db640fd56379857157fc2f79c14f28856
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 18:48:23 -0400

    separated out result list item

--- src/features/results/ResultsList.js
+++ src/features/results/ResultsList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectAllResults } from '../search/searchSlice';
+import ResultItem from './ResultItem';
 
 
 export default function ResultsList () {
   const results = useSelector(selectAllResults);
+
   return (
     <section>
       <h3>Results</h3>
       <ul>
       {results.map((search) => {
-        return <li key={search.objectID}> {search.title} </li>
+        return <ResultItem key={search.objectID} data={search} />
       })}
       </ul>
     </section>
