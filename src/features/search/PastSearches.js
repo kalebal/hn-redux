0d721779d1a4b6e59884c207e26efc0525ac3b53
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 18:05:59 -0400

    Renders list of past search terms from store

--- src/features/search/PastSearches.js
+++ src/features/search/PastSearches.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { selectSearch } from './searchSlice';
 
 export default function PastSearches () {
+  const pastSearches = useSelector(selectSearch);
   return (
-
+    <section>
+      <h3>Past Searches</h3>
+      <ul>
+      {pastSearches.map((search) => {
+        return <li key={search}> {search} </li>
+      })}
+      </ul>
+    </section>
   );
 }
