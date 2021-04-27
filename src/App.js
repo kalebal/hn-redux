b8c49497dea5ee0cbd1dcd7d0304b647d5b7a261
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 18:05:19 -0400

    move components out of header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Search } from './features/search/Search';
+import ResultsList from './features/search/ResultsList';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Search />
-      </header>
+      <Search />
+      <ResultsList />
     </div>
   );
 }
