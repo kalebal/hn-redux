e53a70b783cdf9402f804eb429aa0a9e68692f20
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 21:24:55 -0400

    Handled undefined state, explicitly defines selector

--- src/features/results/ResultsList.js
+++ src/features/results/ResultsList.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectAllResults } from '../search/searchSlice';
 import ResultItem from './ResultItem';
 
 
+const selectAllResults = state => state.results.currentSearchResults;
 export default function ResultsList () {
   const results = useSelector(selectAllResults);
 
-  return (
-    <section>
-      <h3>Results</h3>
-      <ul>
-      {results.map((result) => {
-        return <ResultItem key={result.objectID} data={result} />
-      })}
-      </ul>
-    </section>
-  );
+  if (typeof results !== undefined) {
+    return (
+      <section>
+        <h3>Results</h3>
+        <ul>
+        {results.map((result) => {
+          return <ResultItem key={result.objectID} data={result} />
+        })}
+        </ul>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h3>Results</h3>
+      </section>
+    );
+  }
 }
