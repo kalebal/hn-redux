b66e438bd81ecd6c461baac19c67e6e4a92f28bb
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 22:50:44 -0400

    Create async middleware, hook up devtools

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux'
-import appReducer from './reducer'
+import { createStore, applyMiddleware } from 'redux';
+import appReducer from './reducer';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(appReducer);
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const store = createStore(appReducer, composedEnhancer);
 
 export default store;
 
