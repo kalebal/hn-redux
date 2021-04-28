962ac226768cbfc6f8788ba10d66bf75f19831b7
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 22:55:50 -0400

    Pluralization matters, comment graveyards? Not as much

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -15,7 +15,7 @@ function nextSearchId(searches) {
 
 export default function searchReducer(state = initialState, action) {
   switch (action.type) {
-    case 'search/searchAdded': {
+    case 'searches/searchAdded': {
       return {
         ...state,
         pastSearches: [
@@ -29,55 +29,5 @@ export default function searchReducer(state = initialState, action) {
     }
     default:
       return state;
+    }
   }
-
-}
-
-// export const fetchResults = createAsyncThunk(
-//   'search/fetchResults',
-//   async (str) => {
-//     const response = await axios.get('http://hn.algolia.com/api/v1/search', {params: { query: str }})
-//     .then((res) => {
-//       return res;
-//     });
-//     // becomes the `fulfilled` action payload
-//     return response.data.hits;
-//   }
-// );
-
-// export const searchSlice = createSlice({
-//   name: 'search',
-//   initialState,
-//   reducers: {
-//     searchSubmitted(state, action) {
-//         state.pastSearches.push(action.payload);
-//       }
-//   },
-//   // extraReduces handles actions created in createAsyncThunk
-//   // TODO: handle promise rejection, loading state UI
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchResults.pending, (state) => {
-//         state.status = 'loading';
-//       })
-//       .addCase(fetchResults.fulfilled, (state, action) => {
-//         state.status = 'idle';
-//         state.searchResults = action.payload;
-//       })
-//       .addCase(fetchResults.rejected, (state, action) => {
-//         state.status = 'failed';
-//         state.error = action.error.message;
-//         state.searchResults = state.searchResults.concat('ERROR');
-//       })
-//   },
-// });
-
-
-// export const { searchSubmitted } = searchSlice.actions;
-
-// export const selectSearch = (state) => state.search.pastSearches;
-// // This grabs all search results, not entirely necessary but included
-// // for readability and potential future filtering/sorting functionality
-// export const selectAllResults = (state) => state.search.searchResults;
-
-// export default searchSlice.reducer;
