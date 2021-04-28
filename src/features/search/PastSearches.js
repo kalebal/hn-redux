ca3e71551fe3ca171a1bbaaa5bb0719d01bd84b1
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 21:25:21 -0400

    explicitly define selector

--- src/features/search/PastSearches.js
+++ src/features/search/PastSearches.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectSearch } from './searchSlice';
 
+const selectSearch = state => state.searches.pastSearches;
 export default function PastSearches () {
   const pastSearches = useSelector(selectSearch);
   // TODO: clicking on a past search changes search input value
