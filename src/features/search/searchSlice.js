4b717b50d46653879dababd70dcabfd7c3c09b6f
Kaleb Lyda <kaleblyda14@gmail.com>
2021-04-27 21:27:02 -0400

    Created reducer from scratch

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   value: '',
@@ -9,52 +7,77 @@ const initialState = {
   error: null,
 };
 
-// Todo: CORS headers
-export const fetchResults = createAsyncThunk(
-  'search/fetchResults',
-  async (str) => {
-    const response = await axios.get('http://hn.algolia.com/api/v1/search', {params: { query: str }})
-    .then((res) => {
-      return res;
-    });
-    // becomes the `fulfilled` action payload
-    return response.data.hits;
-  }
-);
+// get the next ID for element in the past search list
+function nextSearchId(searches) {
+  const maxId = searches.reduce((maxId, search) => Math.max(search.id, maxId), -1)
+  return maxId + 1
+}
 
-export const searchSlice = createSlice({
-  name: 'search',
-  initialState,
-  reducers: {
-    searchSubmitted(state, action) {
-        state.pastSearches.push(action.payload);
+export default function searchReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'search/searchAdded': {
+      return {
+        ...state,
+        pastSearches: [
+          ...state.pastSearches,
+          {
+            id: nextSearchId(state.pastSearches),
+            text: action.payload
+          }
+        ]
       }
-  },
-  // extraReduces handles actions created in createAsyncThunk
-  // TODO: handle promise rejection, loading state UI
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchResults.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(fetchResults.fulfilled, (state, action) => {
-        state.status = 'idle';
-        state.searchResults = action.payload;
-      })
-      .addCase(fetchResults.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-        state.searchResults = state.searchResults.concat('ERROR');
-      })
-  },
-});
+    }
+    default:
+      return state;
+  }
+
+}
+
+// export const fetchResults = createAsyncThunk(
+//   'search/fetchResults',
+//   async (str) => {
+//     const response = await axios.get('http://hn.algolia.com/api/v1/search', {params: { query: str }})
+//     .then((res) => {
+//       return res;
+//     });
+//     // becomes the `fulfilled` action payload
+//     return response.data.hits;
+//   }
+// );
+
+// export const searchSlice = createSlice({
+//   name: 'search',
+//   initialState,
+//   reducers: {
+//     searchSubmitted(state, action) {
+//         state.pastSearches.push(action.payload);
+//       }
+//   },
+//   // extraReduces handles actions created in createAsyncThunk
+//   // TODO: handle promise rejection, loading state UI
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchResults.pending, (state) => {
+//         state.status = 'loading';
+//       })
+//       .addCase(fetchResults.fulfilled, (state, action) => {
+//         state.status = 'idle';
+//         state.searchResults = action.payload;
+//       })
+//       .addCase(fetchResults.rejected, (state, action) => {
+//         state.status = 'failed';
+//         state.error = action.error.message;
+//         state.searchResults = state.searchResults.concat('ERROR');
+//       })
+//   },
+// });
 
 
-export const { searchSubmitted } = searchSlice.actions;
+// export const { searchSubmitted } = searchSlice.actions;
 
-export const selectSearch = (state) => state.search.pastSearches;
-// This grabs all search results, not entirely necessary but included
-// for readability and potential future filtering/sorting functionality
-export const selectAllResults = (state) => state.search.searchResults;
+// export const selectSearch = (state) => state.search.pastSearches;
+// // This grabs all search results, not entirely necessary but included
+// // for readability and potential future filtering/sorting functionality
+// export const selectAllResults = (state) => state.search.searchResults;
 
-export default searchSlice.reducer;
+// export default searchSlice.reducer;
