a693eea4ffc9dfbd20f7827ea75efea30d5972a2
Kaleb Lyda <kaleblyda14@gmail.com>
2021-05-06 16:10:17 -0400

    Remove dev tools

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import appReducer from './reducer';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const composedEnhancer = applyMiddleware(thunkMiddleware);
 const store = createStore(appReducer, composedEnhancer);
 
 export default store;
