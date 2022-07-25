08f75d2ab07a7f46ab6a78899c0d4d17efb1e74e
kalebal <kaleblyda14@gmail.com>
2022-07-25 19:43:23 -0400

    Update styles

--- src/features/search/Search.js
+++ src/features/search/Search.js
@@ -1,44 +1,43 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchResults } from '../results/resultSlice';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { fetchResults } from "../results/resultSlice";
 
 export function Search() {
   const dispatch = useDispatch();
-  const [text, setText] = useState('')
-  const handleChange = e => setText(e.target.value)
+  const [text, setText] = useState("");
+  const handleChange = (e) => setText(e.target.value);
 
-  const handleKeyDown = e => {
-    const trimmedText = e.target.value.trim()
-    if (e.key === 'Enter' && trimmedText) {
+  const handleKeyDown = (e) => {
+    const trimmedText = e.target.value.trim();
+    if (e.key === "Enter" && trimmedText) {
       // create new thunk function with submitted query
       dispatch(fetchResults(text));
       // And clear out the text input
-      setText('')
+      setText("");
     }
-  }
+  };
 
   // To consider: combine handleKeyDown and handleSubmit
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     console.log(text);
     // create new thunk function with submitted query
     dispatch(fetchResults(text));
     // And clear out the text input
-    setText('');
-  }
+    setText("");
+  };
 
   return (
     <div className="searchContainer">
       <div className="searchBar" action="">
         <input
-        type="text"
-        placeholder="What're you looking for? 🧐"
-        autoFocus={true}
-        value={text}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
+          type="text"
+          placeholder="  Search"
+          autoFocus={true}
+          value={text}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
         />
-        <button className="fa fa-search"
-        onClick={handleSubmit} />
+        <button className="fa fa-search" onClick={handleSubmit} />
       </div>
     </div>
   );
